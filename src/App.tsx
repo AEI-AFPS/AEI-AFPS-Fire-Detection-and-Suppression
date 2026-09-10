590b3f9c005f077b2fc3b1b56362331415a24444
Maniteja_Gaddam <happyfamily110705@gmail.com>
2026-09-10 12:24:37 +0530

    [feature addded

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "./components/ui/sonner";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Products from "./pages/Products";
@@ -62,6 +63,7 @@ const App = () => (
         <WhatsAppButton />
       </HashRouter>
     </TooltipProvider>
+    <Analytics />
   </QueryClientProvider>
 );
 
